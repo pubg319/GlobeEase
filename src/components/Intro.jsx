16cec06f885348f3a79677903bb37f6eb01870ee
B X Gate's <75904878+pubg319@users.noreply.github.com>
2024-11-30 14:42:27 +0530

    first commit

---
+++ src/components/Intro.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Intro() {
+   return (
+      <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+         <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">GlobeEase</h1>
+         <p className="text-base md:text-xl mb-3 font-medium">IMMIGRATION SERVICE</p>
+         <p className="text-sm max-w-xl mb-6 font-bold">
+            
+            <br />
+         </p>
+      </div>
+   )
+}
+
+export default Intro;
---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+   {
+      title: 'Visa to UK',
+      imgUrl: '/public/image/canada.png',
+      stack: ['', '', ''],
+      link: 'https://www.instagram.com/p/C87shrjy6LR/',
+   },
+   {
+      title: 'Visa to UK',
+      imgUrl: '/public/image/uk.png',
+      stack: ['', '', ''],
+      link: 'https://thehackershideout.com',
+   },
+   {
+      title: 'Visa to Canada',
+      imgUrl: '/public/image/can1.png',
+      stack: ['', '', ''],
+      link: 'https://youtu.be/A8eegxL82PI',
+   },
+]
